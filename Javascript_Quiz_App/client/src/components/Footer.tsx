df6db9c5416482ebcf95424b52aba30a31fcea02
nick v <nvalente.electrics@gmail.com>
2023-08-17 18:48:30 +0100

    corrected dark mode settings

--- Javascript_Quiz_App/client/src/components/Footer.tsx
+++ Javascript_Quiz_App/client/src/components/Footer.tsx
@@ -25,7 +25,9 @@ export default function Footer() {
               fill="currentColor"
               viewBox="0 0 24 24"
             >
-              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" 
+              fill-rule="evenodd"
+              clip-rule="evenodd"/>
             </svg>
           </a>
           <a
